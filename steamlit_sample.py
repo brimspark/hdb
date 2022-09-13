e788bbff9aa31792069afd159f572835e61ea562
Joel Pang <43898094+JoelPang@users.noreply.github.com>
2022-09-14 00:17:03 +0800

    Add files via upload

---
+++ steamlit_sample.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Crypto Dashboard")
+
+
+st.markdown(
+    """# **Crypto Dashboard**
+A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072), from [Data Professor](https://twitter.com/thedataprof).
+"""
+)
+
+st.header("**Selected Price**")
+
+# Load market data from Binance API
+df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
+
+
+crpytoList = {
+    "Price 1": "BTCBUSD",
+    "Price 2": "ETHBUSD",
+    "Price 3": "BNBBUSD",
+}
+
+col1, col2, col3 = st.columns(3)
+
+for i in range(len(crpytoList.keys())):
+    selected_crypto_label = list(crpytoList.keys())[i]
+    selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+    selected_crypto = st.sidebar.selectbox(
+        selected_crypto_label, df.symbol, selected_crypto_index, key=str(i)
+    )
+    col_df = df[df.symbol == selected_crypto]
+    col_price = col_df.weightedAvgPrice
+    col_percent = f"{float(col_df.priceChangePercent)}%"
+    with col1:
+        st.metric(selected_crypto, col_price, col_percent)
+
+
+st.header("")
+
+
+
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+
+csv = convert_df(df)
+
+
+
+
+
+st.markdown(
+    """
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""",
+    unsafe_allow_html=True,
+)
