36a6fd63fec6f91b4cb4f45387cb40a7a50d5aff
Joel Pang <43898094+JoelPang@users.noreply.github.com>
2022-09-14 09:35:02 +0800

    Update steamlit_sample.py

--- steamlit_sample.py
+++ steamlit_sample.py
@@ -3,18 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Crypto Dashboard")
 
-
-st.markdown(
-    """# **Crypto Dashboard**
-A simple cryptocurrency price app pulling price data from the [Binance API](https://www.binance.com/en/support/faq/360002502072), from [Data Professor](https://twitter.com/thedataprof).
-"""
-)
-
-st.header("**Selected Price**")
-
-# Load market data from Binance API
 df = pd.read_json("https://api.binance.com/api/v3/ticker/24hr")
-
 
 crpytoList = {
     "Price 1": "BTCBUSD",
@@ -38,27 +27,3 @@ for i in range(len(crpytoList.keys())):
 
 
 st.header("")
-
-
-
-
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode("utf-8")
-
-
-csv = convert_df(df)
-
-
-
-
-
-st.markdown(
-    """
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-""",
-    unsafe_allow_html=True,
-)
